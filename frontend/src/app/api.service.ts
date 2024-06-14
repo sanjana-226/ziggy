ac1fade38a293539abe5b5606fd8f367c9375fef
sanjana-226 <69665875+sanjana-226@users.noreply.github.com>
2024-06-15 00:31:09 +0530

    api

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:5000';  
+
+  constructor(private http: HttpClient) { }
+
+  // Orders API
+  getOrders(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/orders`);
+  }
+
+  getOrderById(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/orders/${id}`);
+  }
+
+  createOrder(order: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/orders`, order);
+  }
+
+  updateOrder(id: string, order: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/orders/${id}`, order);
+  }
+
+  deleteOrder(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/orders/${id}`);
+  }
+
+  // Drivers API
+  getDrivers(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/drivers`);
+  }
+
+  getDriverById(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/drivers/${id}`);
+  }
+
+  createDriver(driver: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/drivers`, driver);
+  }
+
+  updateDriver(id: string, driver: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/drivers/${id}`, driver);
+  }
+
+  deleteDriver(id: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/drivers/${id}`);
+  }
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { CreateOrderComponent } from './create-order/create-order.component';
@@ -9,10 +10,12 @@ import { DriversComponent } from './drivers/drivers.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ApiService } from './api.service';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -28,10 +31,12 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

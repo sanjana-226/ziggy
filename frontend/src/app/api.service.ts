68e7cf734899a779b2cbfaf558137a9f391c3b7d
sanjana-226 <69665875+sanjana-226@users.noreply.github.com>
2024-06-15 17:51:34 +0530

    api

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:5000';  
+  private baseUrl = 'https://ziggy-fp72.onrender.com';  
 
   constructor(private http: HttpClient) { }
 
